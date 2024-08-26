02391aa2a433662170646874110ef9177920c2d2
Sean Bailey <sbailey@pve-llc.com>
2024-08-26 16:51:47 -0400

    working: fix client disconnect

--- src/server/app/main.py
+++ src/server/app/main.py
@@ -96,5 +96,5 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
             await manager.send_personal_message(f"You wrote: {data}", websocket)
             await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
-        manager.disconnect(websocket, client_id)
+        manager.disconnect(client)
         await manager.broadcast(f"Client #{client_id} left the chat")
